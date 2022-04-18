d3fb32d7003179d918d55fb3320b95f29be6895e
Luther A Anderson <95545605+lutheranders1@users.noreply.github.com>
2022-04-18 11:05:20 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^yaez)r=sfas1&g7i45b7@jxvv6@ax6$r%#bse*^%))3d40m02'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
